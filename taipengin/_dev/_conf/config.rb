9ee7eb0b428678726f71230937652b37343b0f8a
makoto31 <tamac17@gmail.com>
2013-09-03 23:31:17 +0900

    設定ファイルをconfフォルダへ移行

--- taipengin/_dev/_conf/config.rb
+++ taipengin/_dev/_conf/config.rb
@@ -2,9 +2,9 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../static/css"
-sass_dir = "sass"
-images_dir = "../static/res"
+css_dir = "../../static/css"
+sass_dir = "../sass"
+images_dir = "../../static/res"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
